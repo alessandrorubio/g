172aa786b76462906a23514e4b2fb167c3652f61
Alessandro Rubio <rubmoa@gmail.com>
2018-10-04 19:13:32 +0200

    Changed test graph

--- src/main.py
+++ src/main.py
@@ -6,8 +6,14 @@ from graph import Graph
 
 
 def main():
-    g = Graph([1, 2, 3, 4], [[2], [3], [4], [2]])
+	g = Graph(
+      [0, 1, 2, 3, 4, 5, 6], 
+      [[1, 3], [0, 2], [1], [0, 4], [3], [6], [5]]
+      )
     print(g)
+    print(g.bfs(2))
+    print(g.bfs_path(1))
+    print(g.ucc())
 
 
 if __name__== '__main__':
