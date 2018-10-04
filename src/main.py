958d529b93ce2ac958f2bd193c7d39dc0b330954
Alessandro Rubio <rubmoa@gmail.com>
2018-10-04 20:50:57 +0200

    Minimal changes

--- src/main.py
+++ src/main.py
@@ -10,10 +10,12 @@ def main():
       [0, 1, 2, 3, 4, 5, 6], 
       [[1, 3], [0, 2], [1], [0, 4], [3], [6], [5]]
       )
-    print(g)
-    print(g.bfs(2))
-    print(g.bfs_path(1))
-    print(g.ucc())
+
+	print(g)
+	print(g.bfs(2))
+	print(g.bfs_path(1))
+	print(g.ucc())
+	print(g.dfs(0))
 
 
 if __name__== '__main__':
