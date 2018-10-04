d05bc6ecf264cf83e8b849ca4bb155ad65a26bda
Alessandro Rubio <rubmoa@gmail.com>
2018-10-04 19:12:44 +0200

    Implemented bfs, bfs_path and ucc

--- src/graph.py
+++ src/graph.py
@@ -23,4 +23,90 @@ class Graph:
             ans += str(self.v[i]) + ": " + str(self.e[i]) + "\n"
         return ans
 
-
+   
+    # TODO: Create a Queue class
+    def bfs(self, s):
+      """
+      Vanilla version of the BFS algorithm, each iteration explores a new vertex that is connected to s.
+      M corresponds to the adjacency list of visited vertices.
+      Q is a Queue.
+      s is the starting point vertex.
+      It returns the M.
+      """
+      M = [0 for v in self.v]
+      M[s] = 1
+      Q = [s]
+      while Q:
+        w = Q[0]
+        Q.remove(w)
+        for i in self.e[w]:
+          if M[i] == 0:
+            M[i] = 1
+            Q.append(i)
+      return M
+    
+    def bfs_path(self, s):
+      """
+      Path version of the BFS algorithm. This versions returns an array P which every vertex corresponds to the distance between s and v.
+      Q is a Queue type data structure.
+      M corresponds to the adjacency list of visited vertices.
+      """
+      M = [0 for v in self.v]
+      P = [0 for v in self.v]
+      M[s] = 1
+      distance = 0
+      P[s] = 0
+      Q = [s]
+      
+      # BUG: With the following test graph:
+      # v = [0, 1, 2, 3, 4, 5, 6], 
+      # e = [[1, 3], [0, 2], [1], [0, 4], [3], [6], [5]]
+      # Calling bfs_path(0) returns P = [0, 1, 2, 1, 3, 0, 0]
+      # but the right answer is P = [0, 1, 2, 1, 2, 0, 0]
+      # it may be because the moment when it reaches vertex 2
+      # the variable distance is changed.
+      while Q:
+        distance += 1
+        w = Q[0]
+        Q.remove(w)
+        for i in self.e[w]:
+          if M[i] == 0:
+            M[i] = 1
+            P[i] = distance
+            Q.append(i)
+      return P
+    
+    # CS
+    def ucc(self):
+      """
+      UCC stands for Undirected Connected Components. ucc() returns an
+      array CC where each index corresponds to a connected component.
+      Q is a Queue type data structure.
+      M corresponds to the adjacency list of visited vertices.
+      """
+      M = [0 for v in self.v]
+      CC = [0 for v in self.v]
+      numCC = 0
+      for i in self.v:
+        if M[i] == 0:
+          numCC += 1
+          Q = [i]
+          while Q:
+            w = Q[0]
+            Q.remove(w)
+            CC[w] = numCC
+            for edge in self.e[w]:
+              if M[edge] == 0:
+                M[edge] = 1
+                Q.append(edge)
+      return CC
+    
+    # TODO: Implement Stack class
+    def dfs(self, s):
+      """
+      Vanilla version of the DFS algorithms. dfs() returns an array M which corresponds to the adjancecy list of visited/explored vertices. The difference between 
+      dfs() and bfs() is the way it explores the graph and the data structure used
+      to iterate between the vertices.
+      S is a stack data structure
+      """
+      pass
