e764a7cd7eb69532f13b78b141a5d0787970e446
Alessandro Rubio <rubmoa@gmail.com>
2018-10-05 21:12:08 +0200

    added a few more graphs

--- src/main.py
+++ src/main.py
@@ -8,14 +8,24 @@ from graph import Graph
 def main():
 	g = Graph(
       [0, 1, 2, 3, 4, 5, 6], 
-      [[1, 3], [0, 2], [1], [0, 4], [3], [6], [5]]
+      [[1, 3], [0, 2], [1], [0, 4], [3, 5], [4, 6], [5]]
       )
 
-	print(g)
-	print(g.bfs(2))
-	print(g.bfs_path(0))
-	print(g.ucc())
-	print(g.dfs(0))
+	g2 = Graph(
+		[0, 1, 2, 3],
+
+		[[1, 2], [], [3], []]
+		)
+
+	g3 = Graph(
+		[0, 1, 2, 3, 4],
+		[[1, 3], [4], [0, 3], [1, 3], []])
+
+	print(g2)
+	print(g2.topoSort())
+	print("- - - - - - - - -")
+	print(g3)
+	print(g3.topoSort())
 
 
 if __name__== '__main__':
