151110aaa59a931f10446a937f296d866f485c1b
Alessandro Rubio <rubmoa@gmail.com>
2018-10-06 16:09:39 +0200

    Implemented topological sort & summarised some comments

--- src/graph.py
+++ src/graph.py
@@ -13,6 +13,8 @@ class Graph:
         """
         self.v = v
         self.explored_vertices = set()
+        self.current = len(v)
+        self.top_order = [0 for x in v]
 
     def __repr__(self):
         """
@@ -24,13 +26,23 @@ class Graph:
           ans += str(vertex) + ' -> ' + str(edge) +'\n'
         return ans
 
+    def reset_explored_vertices(self):
+      """
+      Resets explored_vertices' set.
+      """
+      self.explored_vertices = set()
+
+    def reset_current(self):
+      """
+      Resets current to cardinal of v
+      """
+      self.current= len(self.v)
+
     def bfs(self, s):
       """
-      Vanilla version of the BFS algorithm, each iteration 
-      explores a new vertex that is connected to s.
-      Q is a Queue.
+      BFS algorithm, each iteration explores a new vertex that is connected to s.
       s is the starting point vertex.
-      It returns the set of explored vertices.
+      Returns the set of explored vertices.
       """
       self.explored_vertices.add(s)
       Q = [s]
@@ -45,22 +57,21 @@ class Graph:
     
     def bfs_path(self, s):
       """
-      Path version of the BFS algorithm. This version returns an array P which
-      every vertex corresponds to the distance between s and v.
-      Q is a Queue type data structure.
-      M corresponds to the adjacency list of visited vertices.
+      Path version of the BFS algorithm.
+      Returns a list P which every index corresponds
+      to the distance between itself and s.
       """
-      M = [0 for v in self.v]
+      self.reset_explored_vertices()
+      self.explored_vertices.add(s)
       P = [0 for v in self.v]
-      M[s] = 1
       P[s] = 0
       Q = [s]
       while Q:
         w = Q[0]
         Q.remove(w)
-        for i in self.e[w]:
-          if M[i] == 0:
-            M[i] = 1
+        for i in self.v[w]:
+          if i not in self.explored_vertices:
+            self.explored_vertices.add(i)
             P[i] = P[w] + 1
             Q.append(i)
       return P
@@ -68,68 +79,74 @@ class Graph:
     # CS
     def ucc(self):
       """
-      UCC stands for Undirected Connected Components. ucc() returns an
-      array CC where each index corresponds to a connected component.
+      UCC stands for Undirected Connected Components.
       Q is a Queue type data structure.
-      M corresponds to the adjacency list of visited vertices.
+      Returns a list CC where each index corresponds to a connected component.
       """
-      M = [0 for v in self.v]
+      self.reset_explored_vertices()
       CC = [0 for v in self.v]
       numCC = 0
       for i in self.v:
-        if M[i] == 0:
+        if i not in self.explored_vertices:
           numCC += 1
           Q = [i]
           while Q:
             w = Q[0]
             Q.remove(w)
             CC[w] = numCC
-            for edge in self.e[w]:
-              if M[edge] == 0:
-                M[edge] = 1
+            for edge in self.v[w]:
+              if edge not in self.explored_vertices:
+                self.explored_vertices.add(edge)
                 Q.append(edge)
       return CC
     
 
     def dfs(self, s):
       """
-      Vanilla version of the DFS algorithm. dfs() returns an array M
-      which corresponds to the adjancecy list of visited/explored vertices.
-      The difference between dfs() and bfs() is the way it explores the graph 
-      and the data structure usedto iterate between the vertices.
-      S is a stack data structure. Using append() and pop() to simulate it.
+      Iterative version of the DFS algorithm. 
+      S is a stack data structure; Using append() and pop() to simulate it.
+      Returns a set containing every explored vertex.
       """
-      M = [0 for v in self.v]
+      self.reset_explored_vertices()
       S = [s]
       while S:
         v = S.pop()
-        if M[v] == 0:
-            M[v] = 1
-            for i in self.e[v]:
-                S.append(i)
-      return M
+        if v not in self.explored_vertices:
+            self.explored_vertices.add(v)
+            for w in self.v[v]:
+                S.append(w)
+      return self.explored_vertices
 
-    def topoSort(self):
-        """
-        Topological sorting using DFS algorithm. It returns
-        a list P which every index corresponds to the order of the
-        topological sort (from 1 to n).
-        """
-        M = [0 for v in self.v]
-        P = [0 for v in self.v]
-        S = []
-        Q = []
-        current = len(self.v)
+    def dfs_rec(self, s):
+      """
+      Recursive version of the DFS algorithm. Returns a set
+      containing all the explored vertices.
+      """
+      self.explored_vertices.add(s)
+      for edge in self.v[s]:
+        if edge not in self.explored_vertices:
+          self.dfs_rec(edge)
+      return self.explored_vertices
 
-        for vertex in self.v:
-            if M[vertex] == 0:
-                S.append(vertex)
-                while S:
-                    v = S.pop()
-                    if M[v] == 0:
-                        M[v] = 1
-                        for w in self.e[v]:
-                            S.append(w)
-                        P[v] = current
-                        current -= 1
-        return P[::-1]
+    def dfs_topo(self, s):
+      """
+      DFS subroutine to be used in topo_sort() only.
+      """
+      self.explored_vertices.add(s)
+      for v in self.v[s]:
+        if v not in self.explored_vertices:
+          self.dfs_topo(v)
+      self.top_order[s] = self.current
+      self.current -= 1
+
+    def topo_sort(self):
+      """
+      Returns a topological sorting of the graph.
+      """
+      self.reset_explored_vertices()
+      self.reset_current()
+
+      for v in self.v:
+        if v not in self.explored_vertices:
+          self.dfs_topo(v)
+      return self.top_order
