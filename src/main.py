a13adbde4f39df4148a8d79518d3481adde46525
Alessandro Rubio <rubmoa@gmail.com>
2018-10-06 13:58:38 +0200

    modified graphs

--- src/main.py
+++ src/main.py
@@ -6,26 +6,17 @@ from graph import Graph
 
 
 def main():
-	g = Graph(
-      [0, 1, 2, 3, 4, 5, 6], 
-      [[1, 3], [0, 2], [1], [0, 4], [3, 5], [4, 6], [5]]
-      )
+	g = {
+	1 : [2, 3],
+	2 : [1, 4],
+	3 : [1, 4],
+	4 : [2, 3]
+	}
 
-	g2 = Graph(
-		[0, 1, 2, 3],
-
-		[[1, 2], [], [3], []]
-		)
-
-	g3 = Graph(
-		[0, 1, 2, 3, 4],
-		[[1, 3], [4], [0, 3], [1, 3], []])
-
-	print(g2)
-	print(g2.topoSort())
-	print("- - - - - - - - -")
-	print(g3)
-	print(g3.topoSort())
+	g1 = Graph(g)
+	print(g1)
+	print('BFS from 2')
+	print(g1.bfs(2))
 
 
 if __name__== '__main__':
